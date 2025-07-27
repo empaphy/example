bcbba2d05fbb0dc42cfbb87a713d72789e4ea37b
Alwin Garside <alwin@garsi.de>
2025-07-27 09:16:54 +0200

    ide: add stubs

---
+++ stubs/PHPStan/dumpType.stub.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPStan;
+
+use JetBrains\PhpStorm\Pure;
+
+/**
+ * @param  mixed  $value
+ * @return void
+ */
+function dumpType(mixed $value): void {}
+
+/**
+ * @param  mixed  $value
+ * @return void
+ */
+function dumpNativeType(mixed $value): void {}
+
+/**
+ * @param  mixed  $value
+ * @return void
+ */
+function dumpPhpDocType(mixed $value): void {}
